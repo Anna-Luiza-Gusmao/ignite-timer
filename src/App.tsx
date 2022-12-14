b6eb5663cfcd286bfc2bece9cd8d6fa70637bc62
anna-luiza-haus <annaluiza@estudiohaus.com>
2022-12-14 09:57:53 -0300

    AddTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
-    <h1>Page home</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Page home</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
